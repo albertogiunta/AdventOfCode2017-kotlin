a1612f4684b9d62d3a0d7a0ab474830872d4dda0
albertogiunta <alb.giunta@gmail.com>
2017-12-09 11:23:55 +0100

    day9

---
+++ src/main/kotlin/day9.kt
@@ -0,0 +1,60 @@
+fun main(args: Array<String>) {
+
+    var chars = readInputFromFile("day9input.txt").split("")
+
+    chars = chars.subList(1, chars.size - 1)
+
+    var opens = mutableListOf<Int>()
+    var canceledGarbage = 0
+
+    fun findResult() {
+
+        var currLevelScore = 1
+        var insideGarbage = false
+        var nextIsInvalid = false
+
+        // It would have been "better" if a normal for loop was used: that way I could've removed all the "nextIsInvalid = false",
+        // cause it would've been enough to increment the "i" of the loop and just skip to the next next element
+
+        chars.forEach {
+            when (it) {
+                "!" -> {
+                    if (insideGarbage) nextIsInvalid = !nextIsInvalid
+                }
+                "{" -> {
+                    if (!nextIsInvalid) {
+                        if (!insideGarbage) opens.add(currLevelScore++)
+                        else canceledGarbage++
+                    }
+                    nextIsInvalid = false
+                }
+                "}" -> {
+                    if (!nextIsInvalid) {
+                        if (!insideGarbage) currLevelScore--
+                        else canceledGarbage++
+                    }
+                    nextIsInvalid = false
+                }
+                "<" -> {
+                    if (!nextIsInvalid) {
+                        if (!insideGarbage) insideGarbage = true
+                        else canceledGarbage++
+                    }
+                    nextIsInvalid = false
+                }
+                ">" -> {
+                    if (!nextIsInvalid) if (insideGarbage) insideGarbage = false
+                    nextIsInvalid = false
+                }
+                else -> {
+                    if (!nextIsInvalid && insideGarbage) canceledGarbage++
+                    nextIsInvalid = false
+                }
+            }
+        }
+    }
+
+    findResult()
+    println("The total score is ${opens.sum()}")
+    println("The total number of canceled items is $canceledGarbage")
+}
