b9cc6c6410220813936fed54eee876e77289eb29
albertogiunta <alb.giunta@gmail.com>
2017-12-13 00:05:32 +0100

    day11 better solution

--- src/main/kotlin/day11.kt
+++ src/main/kotlin/day11.kt
@@ -1,33 +1,41 @@
+import kotlin.math.abs
+
 fun main(args: Array<String>) {
 
     val directions = readInputFromFile("day11input.txt").split(",")
 
-    var n = directions.count { it == "n" }
-    var s = directions.count { it == "s" }
-    var se = directions.count { it == "se" }
-    var nw = directions.count { it == "nw" }
-    var ne = directions.count { it == "ne" }
-    var sw = directions.count { it == "sw" }
+    var dist = mutableListOf<Int>()
+    var x = 0
+    var y = 0
+    var z = 0
 
-    fun compareAndAssignWithZero(a: Int, b: Int): Pair<Int, Int> = if (a > b) Pair(a - b, 0) else Pair(0, b - a)
+    directions.forEach {
+        when (it) {
+            "n" -> {
+                y++; z--
+            }
+            "s" -> {
+                z++; y--
+            }
+            "ne" -> {
+                z--; x++
+            }
+            "nw" -> {
+                y++; x--
+            }
+            "se" -> {
+                x++; y--
+            }
+            "sw" -> {
+                x--; z++
+            }
+        }
+        dist.add((abs(x) + abs(y) + abs(z)) / 2)
+    }
 
-    val (tempN, tempS) = compareAndAssignWithZero(n, s)
-    n = tempN
-    s = tempS
-
-    val (tempNE, tempSW) = compareAndAssignWithZero(ne, sw)
-    ne = tempNE
-    sw = tempSW
-
-    val (tempNW, tempSE) = compareAndAssignWithZero(nw, se)
-    nw = tempNW
-    se = tempSE
-
-    if (nw > ne) nw -= ne else ne -= nw
-    if (sw > se) sw -= se else se -= sw
-
-    val steps = n + nw + ne + s + sw + se
+    val steps = (abs(x) + abs(y) + abs(z)) / 2
 
     println("The least amount of steps required is $steps")
+    println("The furthest of steps is ${dist.max()}")
 
 }
